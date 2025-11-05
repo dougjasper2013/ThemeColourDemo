c106c26f4bdce458c78b9b9be0907691eaaae2c5
Douglas Jasper <doug.jasper@trios.com>
2025-11-05 09:11:01 -0500

    Initial Commit

---
+++ ThemeColourDemo/ThemeColourDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeColourDemoApp.swift
+//  ThemeColourDemo
+//
+//  Created by Douglas Jasper on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct ThemeColourDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThemeColourDemoTests/ThemeColourDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeColourDemoTests.swift
+//  ThemeColourDemoTests
+//
+//  Created by Douglas Jasper on 2025-11-05.
+//
+
+import Testing
+@testable import ThemeColourDemo
+
+struct ThemeColourDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
